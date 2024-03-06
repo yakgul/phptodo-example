13816623b2927cb541e8e0494bacf4b67fc54696
ugurk-ebilgi <ugurk@ebilgi.com.tr>
2024-03-06 11:19:07 +0300

    Update Dockerfile and config.php***

--- api/app/config.php
+++ api/app/config.php
@@ -1,5 +1,5 @@
 <?php 
 define("DB_HOST", "phptodo_database");
 define("DB_USERNAME", "todo_admin");
-define("DB_PASSWORD", "password");
+define("DB_PASSWORD",  getenv('DB_PASSWORD'));
 define("DB_NAME", "todo_app");
